30f09a0d5c23ad0724a4dc2a8101c7d60e560c82
Owais-Ovii <ovi.xd.ivo@gmail.com>
2025-04-26 20:50:00 +0530

    Create 8.Positive.nege.zero.via.nested.if.c

---
+++ Prog. for problem./8.Positive.nege.zero.via.nested.if.c
@@ -0,0 +1,31 @@
+// To check whether the input is positive negative or zero using nested if
+
+#include <stdio.h>
+
+int main() {
+
+    float n;
+    printf("Input the value of: n \n");
+    scanf("%f",&n);
+
+    if (n)
+    {
+        if (n>0)
+        {
+            printf("positive");
+        }
+        else if (n<0)
+        {
+           printf("negative");
+
+        }
+        
+    }
+    else{
+        printf("Number is zero");
+    }
+    
+    
+
+    return 0;
+}
