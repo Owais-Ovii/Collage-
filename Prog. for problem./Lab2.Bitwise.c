84e6114c6a94f5582a4afa7053d16a6d86005fae
Owais-Ovii <ovi.xd.ivo@gmail.com>
2025-04-26 21:06:03 +0530

    Create Lab2.Bitwise.c

---
+++ Prog. for problem./Lab2.Bitwise.c
@@ -0,0 +1,21 @@
+/*  
+Bitwise operators. 
+*/
+
+#include <stdio.h>
+#include <stddef.h>
+
+int main() {
+
+    int a=32,b=34;
+    printf("a&b is equal to %d\n",a&b );
+    printf("a|b is equal to %d\n", a|b);
+    printf("a^b is equal to %d \n", a^b);
+    printf("~a is equal to %d\n", ~a);//gives us 2's compliment
+
+    //check right and left bitwise shifts 
+
+
+
+    return 0;
+}
