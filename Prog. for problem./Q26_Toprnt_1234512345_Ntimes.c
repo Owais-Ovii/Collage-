d51971a3a23edc8f31a23cb426ae76452fe92877
Owais-Ovii <ovi.xd.ivo@gmail.com>
2025-05-31 16:02:19 +0530

    Create Q26_Toprnt_1234512345_Ntimes.c

---
+++ Prog. for problem./Q26_Toprnt_1234512345_Ntimes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+int n;
+printf("\nFor seq, Enter value of N: ");
+scanf("%d",&n);
+
+for(int i = 0;i<n;i++){
+    
+    for(int j = 1; j<=5;j++){
+        
+        printf("%d ",j);
+        
+    }
+    
+}
+
+    return 0;
+}
