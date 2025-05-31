fdfbb0ce7155d0b78cf8712b0177f2fefbf7bc17
Owais-Ovii <ovi.xd.ivo@gmail.com>
2025-05-31 19:23:56 +0530

    Create Q31_Toprnt_stars_piramid.c

---
+++ Prog. for problem./Q31_Toprnt_stars_piramid.c
@@ -0,0 +1,28 @@
+/*to print
+*
+**
+***
+*/
+#include<stdio.h>
+int main (){
+
+    int t; //user input
+    scanf("%d", &t);
+    int n=1;
+    int w=0;
+
+    while (n<=t)
+    {
+        while (w<n)
+        {
+        printf ("*");
+        w++;
+        }
+        w=0;
+        printf("\n");
+        n++;
+    }
+    
+
+    return 0;
+}
